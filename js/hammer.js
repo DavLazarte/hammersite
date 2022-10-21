f6f5386e0d8a898cedab6416515625192d00a029
David Lazarte <dav_lazarte@hotmail.com>
2022-10-21 18:27:27 -0300

    slider de fotos y activiades

---
+++ js/hammer.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    autoWidth:true,
+    margin:15,
+    // responsive:{
+    //     600:{
+    //         items:4
+    //     }
+    // }
+});
