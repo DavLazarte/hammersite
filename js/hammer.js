26661146ef4f944757ba88c0d4dc4e169d349f0a
David Lazarte <dav_lazarte@hotmail.com>
2022-11-29 17:47:02 -0300

    retoques de js

--- js/hammer.js
+++ js/hammer.js
@@ -1,12 +1,14 @@
-$('.owl-carousel').owlCarousel({
-    center: true,
-    items:2,
-    loop:false,
-    autoWidth:true,
-    margin:15,
-    // responsive:{
-    //     600:{
-    //         items:4
-    //     }
-    // }
-});
+document.ready(function(){
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        items:2,
+        loop:false,
+        autoWidth:true,
+        margin:15,
+        // responsive:{
+        //     600:{
+        //         items:4
+        //     }
+        // }
+    });
+})
